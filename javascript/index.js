3f4531bf290fc1c4c4d9698235f62e21446dde08
KhunHN <khin.hto79@gmail.com>
2024-10-26 05:48:57 +0200

    Date & Time functions added

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,22 @@
-let torontoElement = document.querySelector("#toronto");
-let torontoDateElement = torontoElement.querySelector(".date");
-let torontoTimeElement = torontoElement.querySelector(".time");
-let torontoTime = moment.tz("America/Toronto");
-torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
-torontoTimeElement.innerHTML = `${torontoTime.format(
-  "h:mm:ss"
-)} <small>${torontoTime.format("A")}</small>`;
+function updateTime() {
+  let torontoElement = document.querySelector("#toronto");
+  let torontoDateElement = torontoElement.querySelector(".date");
+  let torontoTimeElement = torontoElement.querySelector(".time");
+  let torontoTime = moment.tz("America/Toronto");
+  torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
+  torontoTimeElement.innerHTML = `${torontoTime.format(
+    "h:mm:ss"
+  )} <small>${torontoTime.format("A")}</small>`;
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment.tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = `${parisTime.format(
+    "h:mm:ss"
+  )} <small>${parisTime.format("A")}</small>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
