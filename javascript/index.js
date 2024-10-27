55cfae8ccf27446c5c3560f586dedb50ad904301
KhunHN <khin.hto79@gmail.com>
2024-10-27 21:49:52 +0100

    Js modified

--- javascript/index.js
+++ javascript/index.js
@@ -16,7 +16,46 @@ function updateTime() {
   parisTimeElement.innerHTML = `${parisTime.format(
     "h:mm:ss"
   )} <small>${parisTime.format("A")}</small>`;
+
+  let singaporeElement = document.querySelector("#singapore");
+  let singaporeDateElement = singaporeElement.querySelector(".date");
+  let singaporeTimeElement = singaporeElement.querySelector(".time");
+  let singaporeTime = moment.tz("Asia/Singapore");
+  singaporeDateElement.innerHTML = singaporeTime.format("MMMM Do YYYY");
+  singaporeTimeElement.innerHTML = `${singaporeTime.format(
+    "h:mm:ss"
+  )} <small>${singaporeTime.format("A")}</small>`;
+
+  let yangonElement = document.querySelector("#yangon");
+  let yangonDateElement = yangonElement.querySelector(".date");
+  let yangonTimeElement = yangonElement.querySelector(".time");
+  let yangonTime = moment.tz("Asia/Yangon");
+  yangonDateElement.innerHTML = yangonTime.format("MMMM Do YYYY");
+  yangonTimeElement.innerHTML = `${yangonTime.format(
+    "h:mm:ss"
+  )} <small>${yangonTime.format("A")}</small>`;
+}
+
+function updatecity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = cityTimeZone;
+  citiesElement = `
+  <div>
+          <h2>${cityTimeZone}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    " A"
+  )}</small></div>
+      </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updatecity);
