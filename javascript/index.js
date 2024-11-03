633e7203145cc04c8b1b29d1437038cd53524227
KhunHN <khin.hto79@gmail.com>
2024-11-03 21:24:14 +0100

    js updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,39 +1,48 @@
 function updateTime() {
   let torontoElement = document.querySelector("#toronto");
-  let torontoDateElement = torontoElement.querySelector(".date");
-  let torontoTimeElement = torontoElement.querySelector(".time");
-  let torontoTime = moment.tz("America/Toronto");
-  torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
-  torontoTimeElement.innerHTML = `${torontoTime.format(
-    "h:mm:ss"
-  )} <small>${torontoTime.format("A")}</small>`;
-
+  if (torontoElement) {
+    let torontoDateElement = torontoElement.querySelector(".date");
+    let torontoTimeElement = torontoElement.querySelector(".time");
+    let torontoTime = moment.tz("America/Toronto");
+    torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
+    torontoTimeElement.innerHTML = `${torontoTime.format(
+      "h:mm:ss"
+    )} <small>${torontoTime.format("A")}</small>`;
+  }
+  //
   let parisElement = document.querySelector("#paris");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment.tz("Europe/Paris");
-  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = `${parisTime.format(
-    "h:mm:ss"
-  )} <small>${parisTime.format("A")}</small>`;
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment.tz("Europe/Paris");
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = `${parisTime.format(
+      "h:mm:ss"
+    )} <small>${parisTime.format("A")}</small>`;
+  }
 
+  //
   let singaporeElement = document.querySelector("#singapore");
-  let singaporeDateElement = singaporeElement.querySelector(".date");
-  let singaporeTimeElement = singaporeElement.querySelector(".time");
-  let singaporeTime = moment.tz("Asia/Singapore");
-  singaporeDateElement.innerHTML = singaporeTime.format("MMMM Do YYYY");
-  singaporeTimeElement.innerHTML = `${singaporeTime.format(
-    "h:mm:ss"
-  )} <small>${singaporeTime.format("A")}</small>`;
-
+  if (singaporeElement) {
+    let singaporeDateElement = singaporeElement.querySelector(".date");
+    let singaporeTimeElement = singaporeElement.querySelector(".time");
+    let singaporeTime = moment.tz("Asia/Singapore");
+    singaporeDateElement.innerHTML = singaporeTime.format("MMMM Do YYYY");
+    singaporeTimeElement.innerHTML = `${singaporeTime.format(
+      "h:mm:ss"
+    )} <small>${singaporeTime.format("A")}</small>`;
+  }
+  //
   let yangonElement = document.querySelector("#yangon");
-  let yangonDateElement = yangonElement.querySelector(".date");
-  let yangonTimeElement = yangonElement.querySelector(".time");
-  let yangonTime = moment.tz("Asia/Yangon");
-  yangonDateElement.innerHTML = yangonTime.format("MMMM Do YYYY");
-  yangonTimeElement.innerHTML = `${yangonTime.format(
-    "h:mm:ss"
-  )} <small>${yangonTime.format("A")}</small>`;
+  if (yangonElement) {
+    let yangonDateElement = yangonElement.querySelector(".date");
+    let yangonTimeElement = yangonElement.querySelector(".time");
+    let yangonTime = moment.tz("Asia/Yangon");
+    yangonDateElement.innerHTML = yangonTime.format("MMMM Do YYYY");
+    yangonTimeElement.innerHTML = `${yangonTime.format(
+      "h:mm:ss"
+    )} <small>${yangonTime.format("A")}</small>`;
+  }
 }
 
 function updatecity(event) {
@@ -41,10 +50,10 @@ function updatecity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = cityTimeZone;
-  citiesElement = `
+  citiesElement.innerHTML = `
+  <div class="city">
   <div>
-          <h2>${cityTimeZone}</h2>
+          <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
