13a70a2f4098e823b41687a37753eea60d21a632
KhunHN <khin.hto79@gmail.com>
2024-11-04 05:49:02 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -62,8 +62,8 @@ function updatecity(event) {
         <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
     " A"
   )}</small></div>
-      </div>`
-      <a href="index.html">All cities"</a>;
+      </div>
+      <a href="index.html">All cities"</a>`;
 }
 
 updateTime();
